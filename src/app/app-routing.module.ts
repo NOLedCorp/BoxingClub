ec151c681a3e00ac0701f83e4f5da35a003b360b
Иван Номоконов <171035@edu.fa.ru>
2019-03-15 15:36:17 +0300

    start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent, pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+
+import { ModalModule, BsModalService } from 'ngx-bootstrap';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { SectionsComponent } from './sections/sections.component';
+import { FooterComponent } from './footer/footer.component';
+import { ModalComponent } from './modal/modal.component';
+import { ModalService } from './services/modal.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainPageComponent,
+    SectionsComponent,
+    FooterComponent,
+    ModalComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ModalModule.forRoot()
   ],
-  providers: [],
+  providers: [FormBuilder, HttpClient, ModalService, BsModalService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
